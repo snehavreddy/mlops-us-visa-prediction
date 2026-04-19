877f1dd892a529e523c05b74428848b9c486e39d
snehav <snehavreddy12@gmail.com>
2026-04-19 19:01:43 +0530

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Sneha V",
+    author_email="snehavreddy12@gmail.com",
+    packages=find_packages()
+)
